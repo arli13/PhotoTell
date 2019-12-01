cd7f25eac8e3a737a6d0a37320440ef93dc3f06f
Uğur Ege Aslan <uguregeaslan@gmail.com>
2019-12-01 22:47:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+ // response.send("Hello world!");
+// });
+
+// an attempt for the early social media idea
+
+ /*exports.getStories = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('stories').get()
+        .then(data => {
+            let stories = [];
+            data.forEach(doc => {
+                stories.push(doc.data());
+            });
+            return res.json(stories);
+        })
+        .catch(err => console.error(err));
+ });*/
+ // Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, email, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+    message:message
+  });
+}
+//The part above is made by the codes and instructions of https://github.com/bradtraversy
